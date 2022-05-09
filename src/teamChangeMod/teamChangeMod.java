41c6a6dc5f1d6dd3281ce558abeac4fbec843656
Ilya246 <ilyukarno@gmail.com>
2022-05-09 14:48:13 +0400

    a lot of changes

--- src/teamChangeMod/teamChangeMod.java
+++ src/teamChangeMod/teamChangeMod.java
@@ -1,5 +1,10 @@
 package teamChangeMod;
 
+import arc.Core;
+import arc.Events;
+import arc.math.*;
+import arc.struct.*;
+import arc.util.*;
 import mindustry.game.*;
 import mindustry.game.Team.*;
 import mindustry.game.Teams.*;
@@ -7,27 +12,60 @@ import mindustry.game.EventType.*;
 import mindustry.mod.Plugin.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
-import arc.Core.*;
-import arc.util.*;
-import arc.util.Log;
-import arc.math.*;
-import arc.struct.Seq;
-import arc.Events;
 
 import static mindustry.Vars.*;
 
 public class teamChangeMod extends Plugin{
-    public static float teamChangeTolerance = 1f;
-    public static Team adminTeam = null;
+    public static String configPrefix = "team-";
+
+    public enum Config{
+        allowSpectate("Whether to allow spectating.", true),
+        teamChangeTolerance("Tolerance towards changing team to teams with more players. Higher = more tolerant.", 1.2f);
+
+        public static final Config[] all = values();
+
+        public final Object defaultValue;
+        public String description;
+
+        Config(String description, Object value){
+            this.description = description;
+            this.defaultValue = value;
+            if(!Core.settings.has(getName())) set(value);
+        }
+        public String getName(){
+            return configPrefix + name();
+        }
+        public float f(){
+            return Core.settings.getFloat(getName(), (float)defaultValue);
+        }
+        public boolean b(){
+            return Core.settings.getBool(getName(), (boolean)defaultValue);
+        }
+        public String s(){
+            return Core.settings.get(getName(), defaultValue).toString();
+        }
+        public void set(Object value){
+            Core.settings.put(getName(), value);
+        }
+    }
 
     @Override
     public void init(){
-        Events.on(PlayerJoin.class, e -> {
-            e.player.team(getAssignTeam(e.player));
-        });
-        Events.on(GameOverEvent.class, e -> {
-            adminTeam = null;
-        });
+        netServer.assigner = (p, players) -> {
+            if(state.rules.pvp){
+                ObjectIntMap<Team> teamPlayers = new ObjectIntMap<>();
+                for(Player pl : players){
+                    teamPlayers.put(pl.team(), teamPlayers.get(pl.team(), 0) + 1);
+                }
+                return state.teams.active.copy().shuffle().min(data -> {
+                    if(data.team == Team.derelict || data.cores.isEmpty()){
+                        return Integer.MAX_VALUE;
+                    };
+                    return teamPlayers.get(data.team);
+                }).team;
+            };
+            return state.rules.defaultTeam;
+        };
     }
     public void registerClientCommands(CommandHandler handler){
         handler.<Player>register("team", "[team]", "Change team, use without arguments to show available teams.", (args, player) -> {
@@ -37,25 +75,25 @@ public class teamChangeMod extends Plugin{
                         return t.team.name.equalsIgnoreCase(args[0]);
                     });
                     if(newTeam != null){
-                        if(state.rules.pvp && newTeam.team != Team.derelict && newTeam.team != adminTeam && !(state.rules.waves && newTeam.team == state.rules.waveTeam)){
-                            int[] newTeamPlayers = new int[]{0};
-                            Groups.player.each(p -> {
+                        if(state.rules.pvp && newTeam.team != Team.derelict && !newTeam.cores.isEmpty()){
+                            int newTeamPlayers = 1; // intentionally set to 1 and not 0 to account for new player
+                            for(Player p : Groups.player){
                                 if(p.team() == newTeam.team){
-                                    newTeamPlayers[0]++;
+                                    newTeamPlayers++;
                                 };
-                            });
-                            if(newTeamPlayers[0] / Groups.player.size() * state.teams.active.size <= teamChangeTolerance){
+                            }
+                            if(newTeamPlayers / Groups.player.size() * state.teams.active.size <= Config.teamChangeTolerance.f()){
                                 player.clearUnit();
                                 player.team(newTeam.team);
-                                Call.sendMessage("Player " + player.name + " [white]changed team to [#" + newTeam.team.color + "]" + newTeam.team.name + "[].");
+                                Call.sendMessage("[accent]Player " + player.coloredName() + " [accent]changed team to [#" + newTeam.team.color + "]" + newTeam.team.name + "[].");
                             }else{
-                                player.sendMessage("That team has too much players.");
+                                player.sendMessage("[scarlet]That team has too much players.");
                             };
                         }else{
-                            player.sendMessage("You can't switch to that team.");
+                            player.sendMessage("[scarlet]You can't switch to that team.");
                         };
                     }else{
-                        player.sendMessage("Couldn't find that team.");
+                        player.sendMessage("[scarlet]Couldn't find that team.");
                     };
                 }else{
                     String[] teams = new String[]{""};
@@ -65,10 +103,10 @@ public class teamChangeMod extends Plugin{
                     player.sendMessage(teams[0]);
                 };
             }catch(Exception badArguments){
-                player.sendMessage("Something went wrong.");
+                player.sendMessage("[scarlet]Something went wrong.");
             };
         });
-        handler.<Player>register("forceteam", "[team] [player...]", "Change team forcibly, usable by admins only. Switches typer if target not provided.", (args, player) -> {
+        handler.<Player>register("forceteam", "[team] [player...]", "Change team forcibly. Switches user if target not provided. [red]Admin only.", (args, player) -> {
             try{
                 if(player.admin){
                     if(args.length >= 1){
@@ -90,18 +128,18 @@ public class teamChangeMod extends Plugin{
                                 if(switchPlayer != null){
                                     switchPlayer.clearUnit();
                                     switchPlayer.team(newTeam);
-                                    switchPlayer.sendMessage("An admin has changed your team to [#" + newTeam.color + "]" + newTeam.name + "[].");
-                                    player.sendMessage("Changed team of player " + switchPlayer.name + " [white]to [#" + newTeam.color + "]" + newTeam.name + "[].");
+                                    switchPlayer.sendMessage("[accent]An admin has changed your team to [#" + newTeam.color + "]" + newTeam.name + "[].");
+                                    player.sendMessage("[accent]Changed team of player " + switchPlayer.name + " [white]to [#" + newTeam.color + "]" + newTeam.name + "[].");
                                 }else{
-                                    player.sendMessage("Couldn't find that player.");
+                                    player.sendMessage("[scarlet]Couldn't find that player.");
                                 };
                             }else{
                                 player.clearUnit();
                                 player.team(newTeam);
-                                player.sendMessage("Changed team to [#" + newTeam.color + "]" + newTeam.name +"[].");
+                                player.sendMessage("[accent]Changed team to [#" + newTeam.color + "]" + newTeam.name +"[].");
                             };
                         }else{
-                            player.sendMessage("Couldn't find that team.");
+                            player.sendMessage("[scarlet]Couldn't find that team.");
                         };
                     }else{
                         String[] teams = new String[]{""};
@@ -111,100 +149,74 @@ public class teamChangeMod extends Plugin{
                         player.sendMessage(teams[0]);
                     };
                 }else{
-                    player.sendMessage("You have to be admin to use this command.");
+                    player.sendMessage("[scarlet]You have to be admin to use this command.");
                 };
             }catch(Exception badArguments){
-                player.sendMessage("Something went wrong.");
+                player.sendMessage("[scarlet]Something went wrong.");
             };
         });
         handler.<Player>register("spectate", "Spectate the game. Use while spectating to stop spectating.", (args, player) -> {
-            if(player.team().active() || player.unit().isNull()){
+            if(!Config.allowSpectate.b()){
+                player.sendMessage("[scarlet]Spectating is disabled on this server.");
+                return;
+            }
+            if(player.team().active() || !player.unit().isNull()){
                 Team newTeam = null;
                 for(Team t : Team.all){
                     if(t.core() == null){
                         newTeam = t;
+                        break;
                     };
                 };
                 if(newTeam != null){
                     player.clearUnit();
                     player.team(newTeam);
-                    Call.sendMessage(player.name + " [white]is now spectating.");
+                    Call.sendMessage(player.coloredName() + " [accent]is now spectating.");
                 }else{
-                    player.sendMessage("Couldn't find a suitable team to switch to in order to spectate. Wait for any team to lose.");
+                    player.sendMessage("[scarlet]Couldn't find a suitable team to switch to in order to spectate. Wait for any team to lose.");
                 };
             }else{
-                player.team(getAssignTeam(player));
-                Call.sendMessage(player.name + " [white]is no longer spectating.");
-            };
-        });
-        handler.<Player>register("forceteamassign", "[team]" ,"Force all admins to be on one team and all players to be on other teams. Input \"reset\" to cancel forceteamassign.", (args, player) -> {
-            try{
-                if(player.admin){
-                    Team newTeam = null;
-                    if(args.length == 1){
-                        if(!args[0].equalsIgnoreCase("reset")){
-                            for(Team t : Team.all){
-                                if(t.name.equalsIgnoreCase(args[0])){
-                                    newTeam = t;
-                                };
-                            };
-                            if(newTeam == null){
-                                player.sendMessage("Couldn't find that team.");
-                            };
-                        }else{
-                            player.sendMessage("Resetting forced team.");
-                        };
-                    }else{
-                        newTeam = player.team();
-                    };
-                    if(newTeam != null || args[0].equalsIgnoreCase("reset")){
-                        adminTeam = newTeam;
-                    };
-                    if(adminTeam != null){
-                        Groups.player.each(p -> {
-                            Team assignTeam = getAssignTeam(p);
-                            p.clearUnit();
-                            p.team(assignTeam);
-                        });
-                        player.sendMessage("Teams forced from [#" + adminTeam.color + "]" + adminTeam.name +"[].");
-                    };
-                }else{
-                    player.sendMessage("You have to be admin to use this command.");
-                };
-            }catch(Exception badArguments){
-                player.sendMessage("Something went wrong.");
+                player.team(netServer.assignTeam(player));
+                Call.sendMessage(player.coloredName() + " [accent]is no longer spectating.");
             };
         });
     }
     public void registerServerCommands(CommandHandler handler){
-        handler.register("teamchangetolerance", "<tolerance>" ,"Changes how tolerant towards team player counts is the team change plugin. Lower = more tolerant. Default is 10.", (args) -> {
+        handler.register("teamconfig", "[name] [value]", "Configure teamChange plugin settings. Run with no arguments to list values.", args -> {
+            if(args.length == 0){
+                Log.info("All config values:");
+                for(Config c : Config.all){
+                    Log.info("&lk| @: @", c.name(), "&lc&fi" + c.s());
+                    Log.info("&lk| | &lw" + c.description);
+                    Log.info("&lk|");
+                }
+                return;
+            }
             try{
-                float newTolerance = Float.parseFloat(args[0]);
-                teamChangeTolerance = newTolerance;
-                Log.info("Changed team change tolerance to " + String.valueOf(newTolerance));
-            }catch(Exception badArgument){
-                Log.info("Bad argument.");
-            };
+                Config c = Config.valueOf(args[0]);
+                if(args.length == 1){
+                    Log.info("'@' is currently @.", c.name(), c.s());
+                }else{
+                    if(args[1].equals("default")){
+                        c.set(c.defaultValue);
+                    }else{
+                        try{
+                            if(c.defaultValue instanceof Float){
+                                c.set(Float.parseFloat(args[1]));
+                            }else{
+                                c.set(Boolean.parseBoolean(args[1]));
+                            }
+                        }catch(NumberFormatException e){
+                            Log.err("Not a valid number: @", args[1]);
+                            return;
+                        }
+                    }
+                    Log.info("@ set to @.", c.name(), c.s());
+                    Core.settings.forceSave();
+                }
+            }catch(IllegalArgumentException e){
+                Log.err("Unknown config: '@'. Run the command with no arguments to get a list of valid configs.", args[0]);
+            }
         });
-    }
-    public Team getAssignTeam(Player p){
-        if(state.rules.pvp){
-            return state.teams.active.min(data -> {
-                if(p.admin && data.team == adminTeam){
-                    return -1;
-                };
-                if(data.team == Team.derelict || data.team == adminTeam || (state.rules.waves && data.team == state.rules.waveTeam)){
-                    return Integer.MAX_VALUE;
-                };
-                int teamPlayers = 0;
-                for(Player pl : Groups.player){
-                    if(pl != p && pl.team() == data.team){
-                        teamPlayers++;
-                    };
-                };
-                return teamPlayers;
-            }).team;
-        };
-        return state.rules.defaultTeam;
     }
 }
